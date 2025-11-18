7969db4dc3544da31e13f3bdcddd59a1d8b06832
Raushan Kumar <sraushan890@gamil.com>
2025-11-19 00:26:41 +0530

    quiz

---
+++ DSA/sort.js
@@ -0,0 +1,10 @@
+let array = [1, 3, 4, 5, 7, 8, 9, 13, 14];
+let array2 = [2, 6, 10, 11, 12, 15, 16, 17, 18];
+
+function mergeSortedArrays(arr1, arr2) {
+  let merged = [];
+  let i = 0;
+  let j = 0;
+}
+
+mergeSortedArrays(array, array2);
