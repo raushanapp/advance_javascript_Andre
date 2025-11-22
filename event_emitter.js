9f7111e77b92b3e06b55d2d7e758bdfeb339de7c
Raushan Kumar <sraushan890@gamil.com>
2025-11-23 00:47:41 +0530

    davance rxjs

--- event_emitter.js
+++ event_emitter.js
@@ -8,6 +8,40 @@ class EventEmitter {
   }
   emit(event, data) {
     if (this.events[event]) {
+      //   console.log("Emit ===>>>", event, data);
+      this.events[event].forEach((cb) => cb(data));
     }
+    // console.log("THIS", this);
+    return this;
   }
 }
+
+export class Logger {
+  log(msg) {
+    console.log(`[LOG]: ${msg}`);
+  }
+}
+
+//  data process class
+
+class DataProcessor {
+  constructor(emitter, logger) {
+    this.emitter = emitter;
+    this.logger = logger;
+    this.emitter.on("process", (data) => {
+      this.logger.log(data.toUpperCase());
+    });
+  }
+
+  process(data) {
+    // console.log("DATA ===", data);
+    this.emitter.emit("process", data);
+  }
+}
+
+const emitter = new EventEmitter();
+const logger = new Logger();
+const processor = new DataProcessor(emitter, logger);
+
+processor.process("hello world");
+emitter.emit("process", "compostion rocks");
