6c778657edb7caabb8e5d67d18eddf9b9d119ed2
Raushan Kumar <sraushan890@gamil.com>
2025-11-26 10:29:20 +0530

    javascripts challenge quiz

---
+++ async_function/index.js
@@ -0,0 +1,27 @@
+//  call stack
+console.log("1");
+console.log("2");
+console.log("3");
+
+const one = () => {
+  const two = () => {
+    console.log("4");
+  };
+  two();
+};
+
+//  call stack
+
+//  Recursion
+
+function foo() {
+  foo();
+}
+
+foo(); // continue runing and fill the heap memory or stack overflow and slow the browser and carsh the browser
+
+//  call stack and aysnc programming
+
+console.log("5");
+setTimeout(() => console.log("6"), 2000);
+console.log("6");
