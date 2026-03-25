45025a86e5f2acfafba94cbd86036c3c20e996b9
Raushan Kumar <sraushan890@gamil.com>
2026-03-25 12:18:14 +0530

    Add initial implementation of 'wird' function to demonstrate global variable creation and strict mode behavior; include comments for clarity.

---
+++ advance/fundamental/execrise.js
@@ -0,0 +1,6 @@
+"use strict";
+function wird() {
+  h = 50;
+  return h;
+}
+wird(); // it will return 50 because javascript will create a global variable h but if i use use strict it will throw an error
