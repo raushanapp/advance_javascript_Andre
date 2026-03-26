4e8ff6e6829f985f327f78007c4709b684216a6a
Raushan Kumar <sraushan890@gamil.com>
2026-03-26 12:40:43 +0530

    Add implementations of hasPairWithSum and containsCommonItem functions to demonstrate pair finding in arrays; include comments on time and space complexity.

--- DSA/find-two-sum.js
+++ DSA/find-two-sum.js
@@ -23,3 +23,35 @@ let nums = [];
 let target = 11;
 const result = findTwoSum(nums, target);
 //  Time complexity is O(n^2) and space complexity is O(1)
+// another approach
+
+function hasPairWithSum(arr, sum) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len; j++) {
+      if (arr[i] + arr[j] === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+hasPairWithSum([1, 2, 3, 4, 5], 9);
+
+//  Better  approch
+
+function hasPairWithSum2(arr, sum) {
+  const mySet = new Set();
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (mySet.has(arr[i])) {
+      return true;
+    }
+    mySet.add(sum - arr[i]);
+    console.log("Sum", mySet);
+  }
+  return false;
+}
+
+hasPairWithSum2([1, 2, 3, 4, 5], 9);
