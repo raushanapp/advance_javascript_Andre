50dd32b9599173c3c6cec937af967c0d4f5e70b0
Raushan Kumar <sraushan890@gmail.com>
2026-03-31 05:07:47 +0530

    Fix variable declaration in 'wird' function to prevent global variable creation; add console logs for getMaxNumber function demonstration.

--- advance/fundamental/execrise.js
+++ advance/fundamental/execrise.js
@@ -1,6 +1,18 @@
 "use strict";
 function wird() {
-  h = 50;
+  var h = 50;
   return h;
 }
 wird(); // it will return 50 because javascript will create a global variable h but if i use use strict it will throw an error
+
+//  call aplly
+
+let arr = [1, 2, 3];
+
+function getMaxNumber(arr) {
+  return Math.max.apply(null, arr);
+}
+
+console.log("====================================");
+console.log(getMaxNumber(arr));
+console.log("====================================");
