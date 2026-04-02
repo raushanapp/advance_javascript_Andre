ea52421285e818dec43d19bb78eb4bc87c9ca316
Raushan Kumar <sraushan890@gmail.com>
2026-04-02 22:02:49 +0530

    Refactor mergeSortArrays and add mergedArrays function for cleaner array merging

--- DSA/Arrays/merge-sort-arrays.js
+++ DSA/Arrays/merge-sort-arrays.js
@@ -8,6 +8,7 @@ function mergeSortArrays(arr1, arr2) {
   //  if array items first then return that second array same goes array item 2
   if (arr1.length === 0) return arr2;
   if (arr2.length === 0) return arr1;
+
   while (arrayItem1 || arrayItem2) {
     if (!arrayItem2 || arrayItem1 < arrayItem2) {
       mergedArrays.push(arrayItem1);
@@ -22,8 +23,20 @@ function mergeSortArrays(arr1, arr2) {
   return mergedArrays;
 }
 
+//  clean code and using javascripts methods
+
+function mergedArrays(arr1, arr2) {
+  if (arr1.length === 0) return arr2;
+  if (arr2.length === 0) return arr1;
+
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+}
+
 // let result = mergeSortArrays([1, 3, 5], [2, 4, 6]);
 let result = mergeSortArrays([-1, 3, 5, 7], [1, 2, 4, 6]);
 // console.log("====================================");
 // console.log(result);
 // console.log("====================================");
+
+let ans = mergedArrays([-1, 3, 5, 7], [1, 2, 4, 6]);
+// console.log(ans);
