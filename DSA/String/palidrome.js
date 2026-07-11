776bdefa0dead614f5927d844c922d641f7e21d0
Raushan Kumar <sraushan890@gmail.com>
2026-07-11 22:47:18 +0530

    DSA Palidrome approche

---
+++ DSA/String/palidrome.js
@@ -0,0 +1,11 @@
+//  finding the palidrome in the given string 
+//  how do i approche this problem here in Brute force we can start first loop from zero index to half of the string
+//  length and second loop should be start from the string last index item to n/2 lenght of string
+
+function findPalidrome(string){
+
+
+	
+}
+
+
