b00f6c3cebc258e33390167b94aaef3d61b6ea90
Raushan Kumar <sraushan890@gmail.com>
2026-08-27 13:46:00 +0530

    Add memory management concepts and examples for call stack and memory leaks

--- excercise/index.js
+++ excercise/index.js
@@ -43,3 +43,51 @@ obj1.a = 30;
 //  Delete keyword
 
 // delete obj1.x = 40;
+
+//  call stack + Memory Heap
+
+const number = 610; // allocate memory for number;
+const string = "some text"; // allocate memory for a string;
+
+const human = {
+  // allocate memory for an object.... and it's value
+  first: "Raushan",
+  last: "Kumar",
+};
+
+function subtractTwo(num) {
+  return num - 2;
+}
+
+function calculate() {
+  const sumTotal = 4 + 5;
+  return subtractTwo(sumTotal);
+}
+
+calculate();
+
+//   call stack
+
+// memory leaks
+
+let array = [];
+for (let i = 5; i > 1; i++) {
+  array.push(i - 1);
+}
+
+//  1. global variable
+
+var a = 1;
+var b = 1;
+var c = 1;
+
+//  event listener
+
+var element = document.getElementById("button");
+
+element.addEventListener("click", onClick);
+//  but never removed the event listener than happen memory leak because user goes back fourth and again and again
+
+// setInterval
+
+setInterval(() => {}, 0);
