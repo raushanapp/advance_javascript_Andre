c525119d53d837e01330a3e8e8f5e7e007cb0e58
Raushan Kumar <sraushan890@gmail.com>
2026-09-07 13:31:15 +0530

    Add detailed notes on CPU cache, bit representation, and JavaScript data types in app.js and DSA.MD

--- DSA/app.js
+++ DSA/app.js
@@ -8,3 +8,38 @@
 // CPU
 //  RAM
 // Storage --->> called perment is called presistent storage is slow
+//  CPU cache  store the current
+// 8 Bit can hold the data --> (11111111) => 255
+// 16 - Bit  can hold the data -->  65,536
+// 32  - Bit  can hold the data 2,147, 483,647
+// 64  - Bit  can hold the data 9,223,372,036,854, 775,807
+
+// | Bits |     Possible values |                     Maximum |
+// | ---: | ------------------: | --------------------------: |
+// |    8 |            2⁸ = 256 |            2⁸ − 1 = **255** |
+// |   16 |        2¹⁶ = 65,536 |        2¹⁶ − 1 = **65,535** |
+// |   32 | 2³² = 4,294,967,296 | 2³² − 1 = **4,294,967,295** |
+// |   64 |                 2⁶⁴ |                     2⁶⁴ − 1 |
+
+// Javascript does not have integer overflow but only have 64 bit and idea is computer can store a certain number of data
+
+Math.pow(6, 1000); // Infinity
+
+//  In Javascript we have
+// primitive data type
+//  1 number
+//  "string"
+//  true --> boolean
+//  undefined
+
+//  Non primitive data types
+//  []
+//  {}
+
+//  Data type Operations
+// 1. Insertion
+// 2. Deletion
+// 3. Traversal
+// 4. Searching
+// 5. Sorting
+// 6. Access
