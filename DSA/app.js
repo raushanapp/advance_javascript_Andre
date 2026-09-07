2f3048988f05884692e17d28130b369d3a619061
Raushan Kumar <sraushan890@gmail.com>
2026-09-07 12:28:01 +0530

    Add initial DSA documentation and basic structure
    - Create DSA.MD with explanations of data structures and algorithms, including definitions, examples, and interview tips.
    - Initialize app.js with comments on data structures and algorithms.
    - Set up index.html to display the data structures content.

---
+++ DSA/app.js
@@ -0,0 +1,10 @@
+// Data Structures
+//  What is Data structures and Alogrithms
+//  waht is Data structure --> Data structure is have a different types of store data according to our need like ,
+//  you use store data like bag,Box Comboard , it's simply means how we store the data
+// How to Build One
+//  How to Use it
+
+// CPU
+//  RAM
+// Storage --->> called perment is called presistent storage is slow
