c2298e39ee8f7c925cf657308a9ca9df296d2118
Raushan Kumar <sraushan890@gmail.com>
2026-09-10 13:16:11 +0530

    Add initial JavaScript basics documentation including data types, comparisons, variables, and variable naming rules

---
+++ basic_javascript/app.js
@@ -0,0 +1,26 @@
+//  Javascript Types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol (new in ECMAScript 6)
+// 7. Object
+
+// Javascript Comparisons
+//  !==
+//  ===
+//  >=
+//  <=
+//  >
+//  <
+
+//  Javascripts Variables
+//  1. var
+//  let (new in ECMAScript 6)
+//  const  (new in ECMAScript 6)
+
+//  Variable Rules
+// 1. --> variable start with small case letter and end with numbers
+// 2. --> you can not start with &percent  and number
+// 3. --> some case you can start with _ underscore and $ dollar sign
